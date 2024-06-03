4a24491979596442c9895189d8d75b0ec8d35909
Ege Yılmaz <egeyilmaz.otonom@gmail.com>
2024-06-03 08:52:11 +0300

    initial commit of the project

---
+++ project_3/CMakeFiles/cloud_loc.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/teamx/udacity/nd0013_cd2693_Exercise_Starter_Code/Lesson_7_Project_Scan_Matching_Localization/c3-project/c3-main.cpp" "CMakeFiles/cloud_loc.dir/c3-main.cpp.o" "gcc" "CMakeFiles/cloud_loc.dir/c3-main.cpp.o.d"
+  "/home/teamx/udacity/nd0013_cd2693_Exercise_Starter_Code/Lesson_7_Project_Scan_Matching_Localization/c3-project/helper.cpp" "CMakeFiles/cloud_loc.dir/helper.cpp.o" "gcc" "CMakeFiles/cloud_loc.dir/helper.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ project_3/run_carla.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# SDL_VIDEODRIVER=offscreen /opt/CARLA_0.9.9.4/CarlaUE4.sh -opengl
+
+/opt/CARLA_0.9.9.4/CarlaUE4.sh -opengl
